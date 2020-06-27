adfe88a54544b7e3730d8847960b1a1a4993235f
danielschwarz1 <daniel.shwz@gmail.com>
2020-06-27 12:15:47 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader?url=false',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/[name].css'
+    })
+  ]
+};
+
+module.exports = config;
